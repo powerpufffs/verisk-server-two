98220ec000f6e7b578fddad14bf8ef7fefc90a1e
Z <asterixandisaac@gmail.com>
2021-03-23 09:13:54 -1000

    hi

--- deploy-ec2.js
+++ deploy-ec2.js
@@ -1,37 +1,47 @@
 const {
-    describeInstances,
-    createInstance,
-    terminateInstance
-} = require("./ec2")
+  describeInstances,
+  createInstance,
+  terminateInstance,
+} = require("./ec2");
 
 const idGenerator = () => {
-    let id = 1000;
-    return () => {
-        id++;
-        return id
-    }
-}
+  let id = 1000;
+  return () => {
+    id++;
+    return id;
+  };
+};
 
-const deploy = ({ ecrUri, tag }) => {
-    const generator = idGenerator() 
-    const base64Script = generateScript(ecrUri, tag)
-    let response = createInstance({
-        imageId: "ami-042e8287309f5df03", 
-        count: 1, 
-        keyName: "verisk-team", 
-        deployId: generator().toString(), 
-        userData: base64Script
-    })
+const deploy = async ({ ecrUri, tag }) => {
+  const generator = idGenerator();
+  const base64Script = generateScript(ecrUri, tag);
 
-    console.log(response);
-    
-    const instances = describeInstances({ })
-    const { PublicDnsName } = instance[0]
-    return PublicDnsName
-}
+  try {
+    let response = await createInstance({
+      imageId: "ami-042e8287309f5df03",
+      count: 1,
+      keyName: "verisk-team",
+      deployId: generator().toString(),
+      userData: base64Script,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
+  console.log(response);
+
+  try {
+    const instances = await describeInstances({});
+  } catch (e) {
+    console.log(e);
+  }
+
+  const { PublicDnsName } = instance[0];
+  return PublicDnsName;
+};
 
 const generateScript = (ECR_URI, tag) => {
-    let command = `#!/bin/bash
+  let command = `#!/bin/bash
     sudo apt-get update
     sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -46,7 +56,7 @@ const generateScript = (ECR_URI, tag) => {
     aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin ${ECR_URI}
     sudo docker run -d -p 8080:8080 ${ECR_URI}/${tag}`;
 
-    return (new Buffer(command)).toString('base64');
-}
+  return new Buffer(command).toString("base64");
+};
 
-module.exports = { deploy }
+module.exports = { deploy };
--- ec2.js
+++ ec2.js
@@ -1,81 +1,92 @@
-const { aws } = require('./aws-config')
+const { aws } = require("./aws-config");
 
 var ec2 = new aws.EC2();
 
-const describeInstances = () => {
-    return ec2.describeInstances(
-        {
-        Filters: [{ Name: "tag:deployID", Values: ["1234"] }],
-        },
-        function(err, data) {
-        if (err) {
-            console.error(err.toString());
-        }
-
-        const reservations = data.Reservations;
-        const instances = reservations.map((reservation) => {
-            const { Instances } = reservation;
-            return Instances
-        });
-        return instances.flat()
-        }
-    );
-}
-
-const createInstance = ({imageId, count, keyName, subnetId="subnet-02fae22e3e7147ba0", securityGroupIds=["sg-056ff00b48bd090df"], deployId, userData}) => {
-    return ec2.runInstances(
-        {
-        ImageId: imageId,
-        MinCount: count,
-        MaxCount: count,
-        KeyName: keyName,
-        TagSpecifications: [
-            {
-                'ResourceType': 'instance',
-                'Tags': [
-                    {
-                        'Key': 'deployId',
-                        'Value': deployId,
-                    },
-                ]
-            },
-        ],
-        SubnetId: subnetId,
-        SecurityGroups: securityGroupIds,
-        InstanceType: "t2.micro",
-        IamInstanceProfile: {
-            'Name': 'verisk-ec2'
-        },
-        UserData: userData
-        },
-        function(err, data) {
-        if (err) {
-            console.error(err.toString());
-        } else {
-            for (var i in data.Instances) {
-            var instance = data.Instances[i];
-            console.log("NEW:\t" + instance.InstanceId);
-            }
-        }
-        }
-    );
-}
-
-const terminateInstance = (instanceId) => {
-    return ec2.terminateInstances({ InstanceIds: [instanceId] }, function(err, data) {
-        if (err) {
+const describeInstances = async () => {
+  return ec2.describeInstances(
+    {
+      Filters: [{ Name: "tag:deployID", Values: ["1234"] }],
+    },
+    function(err, data) {
+      if (err) {
         console.error(err.toString());
-        } else {
-        for (var i in data.TerminatingInstances) {
-            var instance = data.TerminatingInstances[i];
-            console.log("TERM:\t" + instance.InstanceId);
+      }
+
+      const reservations = data.Reservations;
+      const instances = reservations.map((reservation) => {
+        const { Instances } = reservation;
+        return Instances;
+      });
+      return instances.flat();
+    }
+  );
+};
+
+const createInstance = async ({
+  imageId,
+  count,
+  keyName,
+  subnetId = "subnet-02fae22e3e7147ba0",
+  securityGroupIds = ["sg-056ff00b48bd090df"],
+  deployId,
+  userData,
+}) => {
+  return ec2.runInstances(
+    {
+      ImageId: imageId,
+      MinCount: count,
+      MaxCount: count,
+      KeyName: keyName,
+      TagSpecifications: [
+        {
+          ResourceType: "instance",
+          Tags: [
+            {
+              Key: "deployId",
+              Value: deployId,
+            },
+          ],
+        },
+      ],
+      SubnetId: subnetId,
+      SecurityGroups: securityGroupIds,
+      InstanceType: "t2.micro",
+      IamInstanceProfile: {
+        Name: "verisk-ec2",
+      },
+      UserData: userData,
+    },
+    function(err, data) {
+      if (err) {
+        console.error(err.toString());
+      } else {
+        for (var i in data.Instances) {
+          var instance = data.Instances[i];
+          console.log("NEW:\t" + instance.InstanceId);
         }
-        }
-    });
-}
+      }
+    }
+  );
+};
+
+const terminateInstance = async (instanceId) => {
+  return ec2.terminateInstances({ InstanceIds: [instanceId] }, function(
+    err,
+    data
+  ) {
+    if (err) {
+      console.error(err.toString());
+    } else {
+      for (var i in data.TerminatingInstances) {
+        var instance = data.TerminatingInstances[i];
+        console.log("TERM:\t" + instance.InstanceId);
+      }
+    }
+  });
+};
 
 module.exports = {
-    describeInstances,
-    createInstance,
-    terminateInstance
-}
+  describeInstances,
+  createInstance,
+  terminateInstance,
+};
