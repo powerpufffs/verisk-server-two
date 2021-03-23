b011ab5d4026738a2bf70b07288a7d0e251c2e0f
Z <asterixandisaac@gmail.com>
2021-03-23 09:29:50 -1000

    try

--- aws-config.js
+++ aws-config.js
@@ -1,10 +1,10 @@
 var aws = require("aws-sdk");
 
-aws.config.update({
-  accessKeyId: "YOUR_ACCESS_KEY",
-  secretAccessKey: "YOUR_SECRET_KEY",
-  region: "us-west-2",
-});
+// aws.config.update({
+//   accessKeyId: "YOUR_ACCESS_KEY",
+//   secretAccessKey: "YOUR_SECRET_KEY",
+//   region: "us-west-2",
+// });
 
 module.exports = {
     aws
