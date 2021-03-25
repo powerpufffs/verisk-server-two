cc687e149f8ff277e78ef96d6147a02cf3e8475e
Z <asterixandisaac@gmail.com>
2021-03-24 14:12:19 -1000

    hi

--- ec2.js
+++ ec2.js
@@ -3,24 +3,28 @@ const { aws } = require("./aws-config");
 var ec2 = new aws.EC2();
 
 const describeInstances = async ({ filterId }) => {
-  return ec2.describeInstances(
-    {
-      // Filters: [{ Name: "tag:deployID", Values: [deployId] }],
-      Filters: [{ Name: "tag:ec2GroupId", Values: [filterId] }],
-    },
-    function(err, data) {
-      if (err) {
-        console.error(err.toString());
-      }
+  return ec2
+    .describeInstances(
+      {
+        // Filters: [{ Name: "tag:deployID", Values: [deployId] }],
+        Filters: [{ Name: "tag:ec2GroupId", Values: [filterId] }],
+      },
+      function(err, data) {
+        if (err) {
+          console.error(err.toString());
+        }
 
-      const reservations = data.Reservations;
-      const instances = reservations.map((reservation) => {
-        const { Instances } = reservation;
-        return Instances;
-      });
-      return instances.flat();
-    }
-  ).promise();
+        console.log(data);
+
+        const reservations = data.Reservations;
+        const instances = reservations.map((reservation) => {
+          const { Instances } = reservation;
+          return Instances;
+        });
+        return instances.flat();
+      }
+    )
+    .promise();
 };
 
 const createInstance = async ({
@@ -32,62 +36,63 @@ const createInstance = async ({
   deployId,
   userData,
 }) => {
-  return await ec2.runInstances(
-    {
-      ImageId: imageId,
-      MinCount: count,
-      MaxCount: count,
-      KeyName: keyName,
-      TagSpecifications: [
-        {
-          ResourceType: "instance",
-          Tags: [
-            {
-              Key: "deployId",
-              Value: deployId,
-            },
-            {
-              Key: "ec2GroupId",
-              Value: 'EC2_LIVE'
-            }
-          ],
+  return await ec2
+    .runInstances(
+      {
+        ImageId: imageId,
+        MinCount: count,
+        MaxCount: count,
+        KeyName: keyName,
+        TagSpecifications: [
+          {
+            ResourceType: "instance",
+            Tags: [
+              {
+                Key: "deployId",
+                Value: deployId,
+              },
+              {
+                Key: "ec2GroupId",
+                Value: "EC2_LIVE",
+              },
+            ],
+          },
+        ],
+        SubnetId: subnetId,
+        SecurityGroupIds: securityGroupIds,
+        InstanceType: "t2.micro",
+        IamInstanceProfile: {
+          Name: "verisk-ec2",
         },
-      ],
-      SubnetId: subnetId,
-      SecurityGroupIds: securityGroupIds,
-      InstanceType: "t2.micro",
-      IamInstanceProfile: {
-        Name: "verisk-ec2",
+        UserData: userData,
       },
-      UserData: userData,
-    },
-    function(err, data) {
-      if (err) {
-        console.error(err.toString());
-      } else {
-        for (var i in data.Instances) {
-          var instance = data.Instances[i];
-          console.log("NEW:\t" + instance.InstanceId);
+      function(err, data) {
+        if (err) {
+          console.error(err.toString());
+        } else {
+          for (var i in data.Instances) {
+            var instance = data.Instances[i];
+            console.log("NEW:\t" + instance.InstanceId);
+          }
         }
       }
-    }
-  ).promise();
+    )
+    .promise();
 };
 
 const terminateInstance = async (instanceId) => {
-  return await ec2.terminateInstances({ InstanceIds: [instanceId] }, function(
-    err,
-    data
-  ) {
-    if (err) {
-      console.error(err.toString());
-    } else {
-      for (var i in data.TerminatingInstances) {
-        var instance = data.TerminatingInstances[i];
-        console.log("TERM:\t" + instance.InstanceId);
+  return await ec2
+    .terminateInstances({ InstanceIds: [instanceId] }, function(err, data) {
+      if (err) {
+        console.error(err.toString());
+      } else {
+        for (var i in data.TerminatingInstances) {
+          var instance = data.TerminatingInstances[i];
+          console.log("TERM:\t" + instance.InstanceId);
+        }
       }
-    }
-  }).promise();
+    })
+    .promise();
 };
 
 module.exports = {
