773ef10cb3855e923cae7db39199a74639526326
Harrison O <h2oyler@gmail.com>
2021-03-29 10:38:45 -0600

    debug statements

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.post("/deploy-ec2", async (req, res) => {
    const { artifactLocation, ecrURL, name } = {
      artifactLocation: "s3://verisk-trial/models/0/942a0174d2f54888a23dc9269d98d69c/artifacts/model/",
      ecrURL: "383367762271.dkr.ecr.us-east-1.amazonaws.com",
-     name: "d_test4",
+     name: "d_test5",
    };
 
   // Call service to build image and push to ECR
@@ -92,7 +92,7 @@ app.post("/deploy-ec2", async (req, res) => {
 });
 
 app.post("/deploy-webhook", async (req, res) => {
-  const { ecrURL, name } = req.params;
+  const { ecrURL, name } = req.body;
   console.log("Request, ecr, name");
   console.log(req)
   console.log(ecrURL);
@@ -100,8 +100,8 @@ app.post("/deploy-webhook", async (req, res) => {
 
   // Create instance
   try {
-    ecrURL = "383367762271.dkr.ecr.us-east-1.amazonaws.com";
-    name = "d_test4";
+    //ecrURL = "383367762271.dkr.ecr.us-east-1.amazonaws.com";
+    //name = "d_test4";
     await deploy({ ecrURL, name });
     console.log("worked!");
   } catch (e) {
