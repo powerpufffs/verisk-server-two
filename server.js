65177a8548286329d1ef2f3ced1ba46dc699499f
Harrison O <h2oyler@gmail.com>
2021-03-30 19:29:01 -0600

    commented out log statements

--- server.js
+++ server.js
@@ -114,16 +114,16 @@ app.post("/deploy-ec2", async (req, res) => {
 //    name: the name of the ecr repository as well as the name of the docker image
 app.post("/deploy-webhook", async (req, res) => {
   const { ecrUrl, name } = req.body;
-  console.log(ecrUrl);
+  //console.log(ecrUrl);
   var validatedEcrUrl = ecrUrl;
-  console.log("val1")
-  console.log(validatedEcrUrl);
+  //console.log("val1")
+  //console.log(validatedEcrUrl);
 
   if (ecrUrl.charAt(ecrUrl.length - 1) === '/') {
     validatedEcrUrl = ecrUrl.slice(0, ecrUrl.length - 1);
   }
-  console.log("val2");
-  console.log(validatedEcrUrl);
+  //console.log("val2");
+  //console.log(validatedEcrUrl);
 
   // Create instance
   try {
