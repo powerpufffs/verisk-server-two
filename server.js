d4451f45b66e194fae224b0ef91ff2e43ac1b0a5
Harrison O <h2oyler@gmail.com>
2021-04-12 22:10:56 -0600

    Query_model work

--- server.js
+++ server.js
@@ -152,8 +152,8 @@ app.post("/query-model", async (req, res) => {
   try {
     console.log("Before");
     response = await axios.post(url, payload, { headers: {
-            "Content-Type": "application/json",
-            format: "pandas-split",
+            "Content-Type": [ "application/json",
+            "format=pandas-split" ]
           }});
     console.log(response);
     console.log(response.data);
